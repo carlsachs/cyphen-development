5e3714b87919439bc0ff19b9363c7c64d59f486a
carlsachs <sachscarl@gmail.com>
2020-08-28 13:42:41 -0400

    finished UI for userform, working on hover effects

--- crazy8/src/components/forms/SimpleContactUs.js
+++ crazy8/src/components/forms/SimpleContactUs.js
@@ -8,6 +8,9 @@ import {ReactComponent as EmailIcon} from "../../images/emailmethod.svg"
 
 const Container = tw.div`relative`;
 const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
+const Box1 = styled.div`
+  
+`;
 
 const FormContainer = styled.div`
   ${tw`p-10 sm:p-12 md:p-16 bg-blue-500 text-gray-100 rounded-lg relative`}
@@ -18,7 +21,7 @@ const FormContainer = styled.div`
     ${tw`text-3xl sm:text-4xl font-bold`}
   }
   input,textarea {
-    ${tw`w-full bg-transparent text-gray-100 text-base font-medium tracking-wide border-b-2 py-2 text-blue-200 hocus:border-pink-400 focus:outline-none transition duration-200`};
+    ${tw`w-full bg-transparent text-gray-100 text-base font-medium tracking-wide border-b-2 py-2 text-blue-800 hocus:border-blue-800 focus:outline-none transition duration-200`};
 
     ::placeholder {
       ${tw`text-white`}
@@ -32,9 +35,9 @@ const InputContainer = tw.div`relative py-5 mt-6`;
 const Label = tw.label`absolute top-0 left-0 tracking-wide font-semibold text-sm`;
 const Input = tw.input``;
 const TextArea = tw.textarea`h-24 sm:h-full resize-none`;
-const SubmitButton = tw.button`w-full sm:w-32 mt-6 py-3 bg-gray-100 text-primary-500 rounded-full font-bold tracking-wide shadow-lg uppercase text-sm transition duration-300 transform focus:outline-none focus:shadow-outline hover:bg-gray-300 hover:text-primary-700 hocus:-translate-y-px hocus:shadow-xl`;
+const SubmitButton = tw.button`w-full sm:w-32 mt-6 py-3 bg-gray-100 text-blue-500 rounded-full font-bold tracking-wide shadow-lg uppercase text-sm transition duration-300 transform focus:outline-none focus:shadow-outline hover:bg-blue-900 hover:text-blue-100 hocus:-translate-y-px hocus:shadow-xl`;
 
-const SvgDotPattern1 = tw(SvgDotPatternIcon)`absolute bottom-0 right-0 transform translate-y-1/2 translate-x-1/2 -z-10 opacity-50 text-primary-500 fill-current w-24`
+const SvgDotPattern1 = tw(SvgDotPatternIcon)`absolute bottom-0 right-0 transform translate-y-1/2 translate-x-1/2 -z-10 opacity-50 text-blue-500 fill-current w-24`
 
 export default () => {
   return (
@@ -65,7 +68,9 @@ export default () => {
                 <div style={{
                   justifyContent: "center"
                 }}>
-                  <div>
+                  <div style={{
+                    marginLeft: "4%"
+                  }}>
                     <h2 style={{
                       color: "white",
                     }}>Preferred Method</h2>
@@ -81,23 +86,35 @@ export default () => {
                       marginTop: "5%",
                       marginBottom: "5%"
                     }}>
+                    <div style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "6.5%"
+                    }}>
                       <EmailIcon style={{
                         height: "50px",
                         width: "50px",
-                        fill: "white"
+                        fill: "white",
                       }}/>
                     </div>
-                    <div style={{
+                    </div>
+                    <Box1 style={{
                       border: "1px solid white",
                       borderRadius: "10px",
                       height: "100px"
+                    }}>
+                    <div style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "6.5%"
                     }}>
                     <PhoneIcon style={{
                       height: "50px",
                       width: "50px",
                       fill: "white"
                     }}/>
-                      </div>
+                    </div>
+                      </Box1>
                       </div>
                   </div>
                 </Column>
