cb5b33217308dd097b51629425089de92045edff
carlsachs <sachscarl@gmail.com>
2020-08-29 17:22:27 -0400

    incorporated logo into web metadata

--- crazy8/src/pages/ContactUs.js
+++ crazy8/src/pages/ContactUs.js
@@ -3,7 +3,7 @@ import tw from "twin.macro"; //eslint-disable-line
 import { css } from "styled-components/macro"; //eslint-disable-line
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 
-import Header from "components/headers/light";
+import Header from "components/headers/lightInner";
 import Top from "components/forms/TwoColContactUsWithIllustrationFullForm";
 import Footer from "components/footers/MiniCenteredFooter";
 
